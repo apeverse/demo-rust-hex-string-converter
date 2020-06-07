36d082c2376541b5a95c746b2a87d204a2dc2e83
lvqinghao <lvqinghao@163.com>
2020-06-07 17:00:44 +0800

    rlp decode

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() {
     println!("{}", zee);
     println!("0x{}", HexFmt(&packet));
 
+    //https://etherscan.io/getRawTx?tx=0x083cc4af906c0b8b67a630507f695aa0dab2bde84fada412fff608d0ee9ea1ae
     let data = hex!("f8ad830dd98a8502540be40083026739947c2af3a86b4bf47e6ee63ad9bde7b3b0ba7f95da80b844a9059cbb000000000000000000000000b34938746d316e995aa81f9b3f94419a0a41e14300000000000000000000000000000000000000000000026faff2dfe5c524000025a0167bf6ce1f7ecee1e5a414e3622baa14daf6caaf90f498b4fb94b1a91bc79491a0362191d3956065a0e14276dd4810b523e93a786091d27388a2b00b6955f93161").to_vec();
     let rlp_obj = rlp::Rlp::new(&data);
     println!("data: {}", rlp_obj);
